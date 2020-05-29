4d42f42cd2860545383d43df614fca052b650dc4
ydalkilic <ysndlklc1234@gmail.com>
2020-05-29 20:55:53 +0300

    added tokenmiddleware

--- routes/dair/index.js
+++ routes/dair/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router();
 const dairController = require('../../controllers/dairController')
 const dairService = require('../../business/dair/index')
-router.post("/", dairController.index);
-router.post("/create", [...dairService.validation("create")], dairController.store);
-router.get("/:dairId", [...dairService.validation("show")], dairController.show);
-router.post("/update", dairController.update);
-router.post("/delete", dairController.destroy);
+const checkToken = require('../../middleware/auth')
+router.post("/", checkToken, dairController.index);
+router.post("/create", checkToken, [...dairService.validation("create")], dairController.store);
+router.get("/:dairId", checkToken, [...dairService.validation("show")], dairController.show);
+router.post("/update", checkToken, dairController.update);
+router.post("/delete", checkToken, dairController.destroy);
 module.exports = router;
--- routes/user/index.js
+++ routes/user/index.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const userController = require('../../controllers/userController')
 const userService = require('../../business/user/index')
 const checkToken = require('../../middleware/auth')
-router.post("/dairgroup", [checkToken, ...userService.validation("dairgroup")], userController.getdairgroup)
-router.post("/create",[checkToken], userController.store);
-router.post("/", [checkToken],userController.index);
-router.post("/:userId",[checkToken], [...userService.validation("show")], userController.show);
-router.post("/update", [checkToken],userController.update);
-router.post("/delete",[checkToken], userController.destroy);
+router.post("/dairgroup", checkToken, [...userService.validation("dairgroup")], userController.getdairgroup)
+router.post("/create", [checkToken], userController.store);
+router.post("/", [checkToken], userController.index);
+router.post("/:userId", [checkToken], [...userService.validation("show")], userController.show);
+router.post("/update", [checkToken], userController.update);
+router.post("/delete", [checkToken], userController.destroy);
 router.post("/:userId/dair", [checkToken], userController.getdair)
 module.exports = router;
 /* [...userService.validation("getdair")]*/

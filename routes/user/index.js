31602d391aac02076c493a6de2f63e7bff7a31f5
ydalkilic <ysndlklc1234@gmail.com>
2020-05-30 22:10:13 +0300

    added delete middleware

--- routes/user/index.js
+++ routes/user/index.js
@@ -4,10 +4,10 @@ const userController = require('../../controllers/userController')
 const userService = require('../../business/user/index')
 const checkToken = require('../../middleware/auth')
 router.post("/dairgroup", checkToken, [...userService.validation("dairgroup")], userController.getdairgroup)
-router.post("/",  userController.index);
+router.post("/", userController.index);
 router.post("/:userId", [checkToken], [...userService.validation("show")], userController.show);
 router.post("/update", [checkToken], userController.update);
-router.post("/delete", [checkToken], userController.destroy);
+router.post("/delete", [checkToken], [...userService.validation("delete")], userController.destroy);
 router.post("/:userId/dair", [checkToken], userController.getdair)
 module.exports = router;
 /* [...userService.validation("getdair")]*/

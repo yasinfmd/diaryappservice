0236293cebcc4cf6cfc2e5898afc89b365b8223d
ydalkilic <ysndlklc1234@gmail.com>
2020-06-03 14:14:54 +0300

    added redis and testing

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const path = require('path');
 const cors = require('cors')
 const router = require('./routes/index')
 const db = require('./database/mongoose')
-const multer = require('multer')
-const uploadJob = require('./jobs/multipleimageUploadJob')
+/*Test Redis*/
+const redisClient = require('./utils/redisclient')
+/*Test Redis*/
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 app.use(cors())
@@ -18,25 +19,7 @@ app.use("/api/user", router.userRouter)
 app.use("/api/dair", router.dairRouter)
 
 app.use("/test", (req, res, next) => {
-/*    var a = 5 + 20
-    console.log(a)
-    uploadJob({mesaj: "YASİN DLKLC"})
-    upload(req, res, function (err) {
-        console.log("re", req.files)
-        console.log("err", err)
-    })
 
-    res.send("ok")*/
-    /* upload(req, res, function (err) {
-         console.log("burda")
-         //console.log(req.body);
-         //console.log(req.files);
-         if (err) {
-             console.log("error",err)
-             return res.send("Error uploading file.");
-         }
-         res.send("File is uploaded");
-     });*/
 })
 app.use((error, req, res, next) => {
     console.log(error);
@@ -45,10 +28,19 @@ app.use((error, req, res, next) => {
     const data = error.data;
     res.status(status).json({message: message, data: data});
 })
+/*Redis Test*/
+redisClient.on('connect', function () {
+    console.log('Redis client bağlandı');
+});
+redisClient.on('error', function (err) {
+    console.log('Redis Clientda Hata ' + err);
+});
+/*Redis Test*/
 db().then((res) => {
     console.log("connect")
     app.listen(process.env.PORT || 3000)
 }).catch((err) => {
+
     throw new Error(err)
 })
 
